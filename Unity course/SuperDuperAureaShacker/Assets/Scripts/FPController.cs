d45b7f53c9e8fca7831805e171f1d9dbdf4d27f7
Max <iram644@hotmail.com>
2021-02-23 21:59:57 -0600

    Added fps models

---
+++ Unity course/SuperDuperAureaShacker/Assets/Scripts/FPController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPController : MonoBehaviour
+{
+    public Transform camera;
+    public Rigidbody rigidBody;
+
+    // Rotation for the camera
+    public float cameraRotationSpeed = 5f;
+    public float cameraMinimumY = -60f;
+    public float cameraMaximumY = 75f;
+    public float rotationSmoothSpeed = 10f;
+    
+    // movement
+    public float walkSpeed = 9f;
+    public float runSpeed = 14f;
+    public float maxSpeed = 20f;
+    public float jumpPower = 30f;
+
+    public float extraGravity;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+    }
+}
